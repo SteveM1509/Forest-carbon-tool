e6b85faa01b2ac77742b4d0b0a6d3bd120ee3cfe
SteveM1509 <145817012+SteveM1509@users.noreply.github.com>
2024-01-21 17:32:16 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -22,41 +22,108 @@ image1_url = '''
     '''
 st.markdown(image1_url, unsafe_allow_html=True)
 
-
+q1=q2=q3=button=None # Questions
+p=i=n=t=a=v0=None # Parameters
 
 st.markdown("""
             ## Forest Carbon and Market Decision Support Tool
             """)
 
-#left, right = st.columns(2)
-dbh = st.slider('Enter the dbh:',min_value=0,max_value=100)
-price=st.slider('Enter the stumpage price:',min_value=0,max_value=100)
-ag=st.slider('Enter the Annual Growth',min_value=0,max_value=100)
+st.markdown("""#### How often does the cash flow occur during the investment project or rotation length? """)
 
-button=st.button('Calculate')
+st.markdown("#### Is it only once (Single Sum)?")
 
+q1 = st.radio('Choose an option',
+              ['Yes','No'], index=None)
+
+if q1=='Yes':
+    v0=st.number_input('Enter the Present Value of single sum as a number',key='1')
+    i=st.number_input('Enter the interest rate in percentage',key='2')
+    n=st.number_input('Enter the number of interest bearing periods',key='3')
+    if v0 and i and n:
+        button=st.button('Calculate Future Value of Single Sum',key='4', type="primary")
+
+elif q1=='No':
+    st.markdown("#### Is it every year (Annual Series)?")
+    
+    q2= st.radio('Choose an option',
+                  ['Yes','No'], index=None,key='5')
+    
+    if q2=='Yes':
+        st.markdown("#### Does it go on forever or end after certain years?")
+        q3=st.radio("Choose an option",['Goes on forever','Ends in n years'], index=None,key='6')
+        
+        if q3=='Goes on forever':
+            a=st.number_input('Enter the equal annual payment',key='7')
+            i=st.number_input('Enter the interest rate in percentage',key='8')
+            if a and i:
+                button=st.button('Calculate Present Value',key='9', type="primary")
+        elif q3=='Ends in n years':
+            a=st.number_input('Enter the equal annual payment',key='10')
+            i=st.number_input('Enter the interest rate in percentage',key='11')
+            n=st.number_input('Enter the number of interest bearing periods',key='12')
+            if a and i and n:
+                button=st.button('Calculate Present and Future Value of Single Sum',key='13', type="primary")
+    elif q2=='No':
+        st.markdown("#### So, it is periodic")
+        st.markdown("#### Does it go on forever or end after certain years?")
+        q3=st.radio("Choose an option",['Goes on forever','Ends in n years'], index=None,key='14')
+        
+        if q3=='Goes on forever':
+            p=st.number_input('Enter equal periodic payment',key='15')
+            i=st.number_input('Enter the interest rate in percentage',key='16')
+            t=st.number_input('Enter the interval between periodic payment',key='17')
+            if p and i and t:
+                button=st.button('Calculate Present Value',key='18', type="primary")
+        elif q3=='Ends in n years':
+            p=st.number_input('Enter equal periodic payment',key='19')
+            i=st.number_input('Enter the interest rate in percentage',key='20')
+            t=st.number_input('Enter the interval between periodic payment',key='21')
+            n=st.number_input('Enter the number of interest bearing periods',key='22')
+            if p and i and n and t:
+                button=st.button('Calculate Present and Future Value',key='23', type="primary")
 
 IST = pytz.timezone('America/Chicago')
 now=datetime.now(IST)
 now=now.strftime('%Y-%m-%d %H:%M:%S')
 
 if button:
-    formula=price*ag 
+    if (q1,q2,q3)==('Yes',None,None):
+        vn=v0*((1+i)**n)
     
-    if formula>2500:
-        st.markdown(f"#### :green[Value is {formula}]")
-    else:
-        st.markdown(f"#### :red[Value is {formula}]")
-        
-    mydb = mysql.connector.connect(
-        host="sql5.freemysqlhosting.net",
-        user="sql5675822",
-        password="Ks3GGjMHWD",
-        database="sql5675822"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute(f"insert into sample values ('{dbh}','{price}','{ag}','{formula}','{now}')")
-    mydb.commit()
-    mycursor.close()
-
-
+    elif (q1,q2,q3)==('No','Yes','Goes on forever'):
+        v0=a/i
+    
+    elif (q1,q2,q3)==('No','Yes','Ends in n years'):
+        num=a*(((1+i)**n)-1)
+        den=i*((1+i)**n)
+        v0=num/den
+        vn=num/i
+    
+    elif (q1,q2,q3)==('No','No','Goes on forever'):
+        v0=p/(((1+i)**t)-1)
+    
+    elif (q1,q2,q3)==('No','No','Ends in n years'):
+        num=p*(((1+i)**n)-1)
+        den=(((1+i)**t)-1)*((1+i)**n)
+        v0=num/den
+        vn=num/(((1+i)**t)-1)
+    
+    if vn and v0:
+        st.markdown(f'#### :green[The present value is {v0} and the future value is {vn}]')
+    elif vn:
+        st.markdown(f'#### :green[The future value is {vn}]')
+    elif v0:
+        st.markdown(f'#### :green[The present value is {v0}]')
+    
+    
+    # mydb = mysql.connector.connect(
+    #     host="sql5.freemysqlhosting.net",
+    #     user="sql5675822",
+    #     password="Ks3GGjMHWD",
+    #     database="sql5675822"
+    # )
+    # mycursor = mydb.cursor()
+    # mycursor.execute(f"insert into sample values ('{dbh}','{price}','{ag}','{formula}','{now}')")
+    # mydb.commit()
+    # mycursor.close()
