1bc6a69aefd3a40cc2187ae256cc35f607b55dd0
SteveM1509 <145817012+SteveM1509@users.noreply.github.com>
2024-02-04 12:00:57 -0500

    Add files via upload

---
+++ Page1.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 15:22:57 2024
+
+@author: Steve
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+if 'inputs' not in st.session_state:
+    st.session_state['inputs']=[]
+add=submit=None
+
+if st.session_state['inputs']:
+    st.write("### Your selections")
+    temp=st.session_state['inputs'][0]
+    for i in st.session_state['inputs'][1:]:
+        temp=pd.concat([temp,i],axis=1)
+    st.table(temp)
+        
+name=st.text_input('Please enter your name')
+ftype=st.selectbox('Select Forest Type',[1,2,3,4,5],index=None)
+state=st.selectbox('Select State',[1,2,3,4,5],index=None)
+choice=st.selectbox('Select Inventory Data Type',['Age','Avg DBH and Height'],index=None)
+
+if choice=='Age':
+    l,r=st.columns(2)
+    age=l.number_input('Enter the Age')
+    acres=r.number_input('Enter the acres')
+elif choice=='Avg DBH and Height':
+    l2,m2,r2=st.columns(3)
+    dbh=l2.number_input('Enter the dbh')
+    height=m2.number_input('Enter the height')
+    acres=r2.number_input('Enter the acres')
+    
+deferral=st.selectbox('Select the deferral period',list(range(0,125,5)),index=None)
+
+if name and ftype and state and choice and deferral:
+    l3,r3=st.columns(2)
+    submit=l3.button('Submit')
+    add=r3.button('Add another forest type')
+else:
+    st.markdown('**:red[There are missing fields!]**')
+
+if add:
+    d={'Name':[],'Forest Type':[],'State':[],'Inventory Data Type':[],'Age':[],'Acres':[]
+       ,'dbh':[],'height':[],'Deferral Period':[]}
+    d['Name'].append(name)
+    d['Forest Type'].append(ftype)
+    d['State'].append(state)
+    d['Inventory Data Type'].append(choice)
+    if choice=='Age':
+        d['Age'].append(age)
+        d['Acres'].append(acres)
+        d['dbh'].append('Not Selected')
+        d['height'].append('Not Selected')
+    elif choice=='Avg DBH and Height':
+        d['Age'].append('Not Selected')
+        d['Acres'].append(acres)
+        d['dbh'].append(dbh)
+        d['height'].append(height)
+    d['Deferral Period'].append(deferral)
+    d=pd.DataFrame(d)
+    d=d.T
+    d.columns=[f'Choice{len(st.session_state["inputs"])+1}']
+    st.session_state['inputs'].append(d)
+    st.rerun()
+
+if submit:
+    temp=st.session_state['inputs'][0]
+    for i in st.session_state['inputs'][1:]:
+        temp=pd.concat([temp,i],axis=1)
+    temp.to_csv('temp.csv')
+    
+
+
---
+++ Page2.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 18:42:48 2024
+
+@author: Steve
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.markdown('#### Your previous choices')
+prev_choice = pd.read_csv('temp.csv',index_col=0)
+
+submit=None
+
+st.table(prev_choice)
+
+sell = st.selectbox('If you were to sell timber today, who would buy it?',['Sawmill','Pulp','Biomass','None',"I don't know"],index=None)
+hard,soft = st.columns(2)
+
+hard.markdown("### Hardwood")
+soft.markdown("### Softwood")
+
+hard_log = hard.number_input('Enter the log price',key=1)
+hard_x = hard.number_input('Enter the x price',key=2)
+hard_biomass = hard.number_input('Enter the biomass price',key=3)
+hard_stump = hard.number_input('What is the stumpage price',key=4)
+
+soft_log = soft.number_input('Enter the log price')
+soft_x = soft.number_input('Enter the x price')
+soft_biomass = soft.number_input('Enter the biomass price')
+soft_stump = soft.number_input('What is the stumpage price')
+
+roi = st.number_input('Minimum acceptable rate of return in percentage')
+ep = st.number_input('Expected price of carbon credit')
+
+if sell and roi and ep:
+    submit = st.button('Submit')
+else:
+    st.markdown('**:red[There are missing fields!]**')
+
+if submit:
+    st.markdown('Your selections')
+    d={'Buyer':[sell],'Harddwood log price':[hard_log],'Hardwood x price':[hard_x],'Hardwood biomass':[hard_biomass],'Hardwood stumpage price':[hard_stump],
+       'Softwood log price':[soft_log],'Softwood x price':[soft_x],'Softwood biomass price':[soft_biomass],'Softwood stumpage price':[soft_stump],
+       'ROI':[roi],'Expected price':[ep]}
+    d=pd.DataFrame(d)
+    st.table(d.T)
